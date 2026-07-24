2beb53c270a195c87b292e065d5ffea6f123f4db
Leang Chouteau <chouteau.leang@gmail.com>
2026-07-24 08:10:37 +0200

    ImplÃementation Visualisation Kibana

--- front/src/app/organization-details/organization-details.component.ts
+++ front/src/app/organization-details/organization-details.component.ts
@@ -2,8 +2,10 @@ import { AsyncPipe, DatePipe, NgFor, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+
 import { Person, PersonService } from '../person.service';
 import { Organization, OrganizationService } from '../organization.service';
+import { LoggingService } from '../services/logging.service';
 
 @Component({
   selector: 'app-organization-details',
@@ -13,50 +15,146 @@ import { Organization, OrganizationService } from '../organization.service';
   styleUrl: './organization-details.component.css'
 })
 export class OrganizationDetailsComponent implements OnInit {
+
   org: Organization = {
-    id: undefined as (number | undefined),
+    id: undefined,
     name: '',
     createdAt: new Date(),
-    updatedAt: undefined as (Date | undefined),
-    persons: [] as Person[]
+    updatedAt: undefined,
+    persons: []
   };
 
-  isNew: boolean = false;
+  isNew = false;
 
-  constructor(private route: ActivatedRoute, private personService: PersonService, private organizationService: OrganizationService, private router: Router) {
+  constructor(
+    private route: ActivatedRoute,
+    private personService: PersonService,
+    private organizationService: OrganizationService,
+    private router: Router,
+    private loggingService: LoggingService
+  ) {
   }
 
   ngOnInit(): void {
+
     const routeParams = this.route.snapshot.paramMap;
     const orgIdParam = routeParams.get('orgId');
 
     if (orgIdParam === 'new') {
-      this.isNew = true
+
+      this.isNew = true;
+
+      this.loggingService.info(
+        'Création d\'une nouvelle organisation',
+        '/organizations/new'
+      );
+
     } else if (typeof orgIdParam === 'string') {
-      const orgId = parseInt(orgIdParam)
-      this.organizationService.fetchById(orgId).then(org => {
-        this.org = org
-        this.isNew = false
-      })
+
+      const orgId = parseInt(orgIdParam);
+
+      this.loggingService.info(
+        `Chargement de l'organisation ${orgId}`,
+        '/organizations'
+      );
+
+      this.organizationService.fetchById(orgId)
+        .then(org => {
+
+          this.org = org;
+          this.isNew = false;
+
+          this.loggingService.info(
+            `Organisation ${orgId} chargée`,
+            '/organizations'
+          );
+
+        })
+        .catch(error => {
+
+          this.loggingService.error(
+            `Erreur lors du chargement de l'organisation ${orgId}`,
+            '/organizations'
+          );
+
+          console.error(error);
+
+        });
 
     }
+
   }
 
   saveOrg() {
+
+    this.loggingService.info(
+      'Sauvegarde d\'une organisation',
+      '/organizations'
+    );
+
     this.organizationService.save({
       ...this.org
-    }).then(o => {
-      this.org = o
-      if (this.isNew) {
-        this.router.navigate(["organizations", o.id])
-      }
     })
+      .then(o => {
+
+        this.org = o;
+
+        this.loggingService.info(
+          `Organisation sauvegardée (id=${o.id})`,
+          '/organizations'
+        );
+
+        if (this.isNew) {
+          this.router.navigate(['organizations', o.id]);
+        }
+
+      })
+      .catch(error => {
+
+        this.loggingService.error(
+          'Erreur lors de la sauvegarde de l\'organisation',
+          '/organizations'
+        );
+
+        console.error(error);
+
+      });
+
   }
 
   deleteOrg() {
-    if (this.org.id === undefined) return
-    this.organizationService.deleteById(this.org.id).then(() => {
-      this.router.navigate([""])
-    })
+
+    if (this.org.id === undefined) {
+      return;
+    }
+
+    this.loggingService.warn(
+      `Suppression de l'organisation ${this.org.id}`,
+      '/organizations'
+    );
+
+    this.organizationService.deleteById(this.org.id)
+      .then(() => {
+
+        this.loggingService.info(
+          'Organisation supprimée',
+          '/organizations'
+        );
+
+        this.router.navigate(['']);
+
+      })
+      .catch(error => {
+
+        this.loggingService.error(
+          'Erreur lors de la suppression de l\'organisation',
+          '/organizations'
+        );
+
+        console.error(error);
+
+      });
+
   }
+
 }
--- front/src/app/person-details/person-details.component.ts
+++ front/src/app/person-details/person-details.component.ts
@@ -2,8 +2,10 @@ import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+
 import { Person, PersonService } from '../person.service';
 import { Organization, OrganizationService } from '../organization.service';
+import { LoggingService } from '../services/logging.service';
 
 @Component({
   selector: 'app-person-details',
@@ -13,76 +15,247 @@ import { Organization, OrganizationService } from '../organization.service';
   styleUrl: './person-details.component.css'
 })
 export class PersonDetailsComponent implements OnInit {
+
   person: Person = {
-    id: undefined as (number | undefined),
+    id: undefined,
     firstName: '',
     lastName: '',
     phone: '',
     email: '',
     bio: '',
     createdAt: new Date(),
-    updatedAt: undefined as (Date | undefined),
-    organizations: [] as (Organization[])
+    updatedAt: undefined,
+    organizations: []
   };
 
-  organizations: Organization[] = []
+  organizations: Organization[] = [];
   selectedOrganization: Organization | null = null;
-  isNew: boolean = false;
+  isNew = false;
 
-  constructor(private route: ActivatedRoute, private personService: PersonService, private organizationService: OrganizationService, private router: Router) {
-    this.organizationService.fetchAll().then(orgs => this.organizations = orgs)
+  constructor(
+    private route: ActivatedRoute,
+    private personService: PersonService,
+    private organizationService: OrganizationService,
+    private router: Router,
+    private loggingService: LoggingService
+  ) {
+
+    this.organizationService.fetchAll()
+      .then(orgs => {
+        this.organizations = orgs;
+
+        this.loggingService.info(
+          'Liste des organisations chargée',
+          '/persons'
+        );
+      })
+      .catch(error => {
+
+        this.loggingService.error(
+          'Erreur lors du chargement des organisations',
+          '/persons'
+        );
+
+        console.error(error);
+      });
+
   }
 
   ngOnInit(): void {
+
     const routeParams = this.route.snapshot.paramMap;
     const personIdParam = routeParams.get('personId');
 
     if (personIdParam === 'new') {
-      this.isNew = true
+
+      this.isNew = true;
+
+      this.loggingService.info(
+        'Création d\'une nouvelle personne',
+        '/persons/new'
+      );
+
     } else if (typeof personIdParam === 'string') {
-      const personId = parseInt(personIdParam)
-      this.personService.fetchById(personId).then(p => {
-        this.person = p
-        this.isNew = false
-      })
+
+      const personId = parseInt(personIdParam);
+
+      this.loggingService.info(
+        `Chargement de la personne ${personId}`,
+        '/persons'
+      );
+
+      this.personService.fetchById(personId)
+        .then(p => {
+
+          this.person = p;
+          this.isNew = false;
+
+          this.loggingService.info(
+            `Personne ${personId} chargée`,
+            '/persons'
+          );
+
+        })
+        .catch(error => {
+
+          this.loggingService.error(
+            `Erreur lors du chargement de la personne ${personId}`,
+            '/persons'
+          );
+
+          console.error(error);
+
+        });
     }
   }
 
   savePerson() {
+
+    this.loggingService.info(
+      'Sauvegarde d\'une personne',
+      '/persons'
+    );
+
     this.personService.save({
       ...this.person
-    }).then(p => {
-      this.person = p
-      if (this.isNew) {
-        this.router.navigate(["persons", p.id])
-      }
     })
+      .then(p => {
+
+        this.person = p;
+
+        this.loggingService.info(
+          `Personne sauvegardée (id=${p.id})`,
+          '/persons'
+        );
+
+        if (this.isNew) {
+          this.router.navigate(['persons', p.id]);
+        }
+
+      })
+      .catch(error => {
+
+        this.loggingService.error(
+          'Erreur lors de la sauvegarde de la personne',
+          '/persons'
+        );
+
+        console.error(error);
+
+      });
+
   }
 
   deletePerson() {
-    if (this.person.id === undefined) return
-    this.personService.deleteById(this.person.id).then(() => {
-      this.router.navigate([""])
-    })
+
+    if (this.person.id === undefined) {
+      return;
+    }
+
+    this.loggingService.warn(
+      `Suppression de la personne ${this.person.id}`,
+      '/persons'
+    );
+
+    this.personService.deleteById(this.person.id)
+      .then(() => {
+
+        this.loggingService.info(
+          'Personne supprimée',
+          '/persons'
+        );
+
+        this.router.navigate(['']);
+
+      })
+      .catch(error => {
+
+        this.loggingService.error(
+          'Erreur lors de la suppression de la personne',
+          '/persons'
+        );
+
+        console.error(error);
+
+      });
+
   }
 
   addSelectedOrganization() {
-    if (this.selectedOrganization?.id === undefined || this.person.id === undefined) return
-    this.organizationService.addPerson(this.selectedOrganization.id, this.person.id)
-    this.refresh()
+
+    if (
+      this.selectedOrganization?.id === undefined ||
+      this.person.id === undefined
+    ) {
+      return;
+    }
+
+    this.organizationService.addPerson(
+      this.selectedOrganization.id,
+      this.person.id
+    );
+
+    this.loggingService.info(
+      `Organisation ${this.selectedOrganization.id} ajoutée à la personne ${this.person.id}`,
+      '/persons'
+    );
+
+    this.refresh();
+
   }
 
   removeOrganization(org: Organization) {
-    if (org?.id === undefined || this.person.id === undefined) return
-    this.organizationService.removePerson(org.id, this.person.id)
-    this.refresh()
+
+    if (
+      org.id === undefined ||
+      this.person.id === undefined
+    ) {
+      return;
+    }
+
+    this.organizationService.removePerson(
+      org.id,
+      this.person.id
+    );
+
+    this.loggingService.warn(
+      `Organisation ${org.id} retirée de la personne ${this.person.id}`,
+      '/persons'
+    );
+
+    this.refresh();
+
   }
 
   refresh() {
-    if (this.person.id === undefined) return
-    this.personService.fetchById(this.person.id).then(p => {
-      this.person = p
-      this.isNew = false
-    })
+
+    if (this.person.id === undefined) {
+      return;
+    }
+
+    this.personService.fetchById(this.person.id)
+      .then(p => {
+
+        this.person = p;
+        this.isNew = false;
+
+        this.loggingService.info(
+          `Rafraîchissement de la personne ${this.person.id}`,
+          '/persons'
+        );
+
+      })
+      .catch(error => {
+
+        this.loggingService.error(
+          'Erreur lors du rafraîchissement de la personne',
+          '/persons'
+        );
+
+        console.error(error);
+
+      });
+
   }
+
 }
